89f7e0b417a94c2e5ff77ac854df43051a9efd6e
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-27 09:47:37 -0600

    had routes mixed up, fixed

--- models/user.js
+++ models/user.js
@@ -7,34 +7,7 @@ var userSchema = new Schema({
   email: String,
   passwordDigest: String
 });
-// create a new user with a hashed password
-userSchema.statics.createSecure = function (email, password, callback) {
-  var UserModel = this;
-  bcrypt.genSalt(function (err, salt) {
-  console.log('salt: ', salt);  
-    bcrypt.hash(password, salt, function (err, hash) {
 
-      UserModel.create({
-        email: email,
-        passwordDigest: hash
-      }, callback);
-    });
-  });
-};
-
-//authenticate user
-userSchema.statics.authenticate = function (email, password, callback) {
-	this.findOne({email: email}, function (err, foundUser) {
-		console.log(foundUser);
-		if (!foundUser) {
-			console.log('No user with email ' + email);
-		} else if (foundUser.checkPassword(password)) {
-			callback (null, foundUser);
-		} else {
-			callback('Error: incorrect password', null);
-		}
-	});
-};
 
 userSchema.methods.checkPAssword = function (password) {
 	return bcrypt.compareSync(password, this.passwordDigest);
